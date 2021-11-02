79c1d88629117b1a1c7c4cf13c1d151dba559fb3
Thanatos095 <mahad095@gmail.com>
2021-11-02 21:56:36 +0500

    Made jumping a state and handled various features connected with that such as gravity

--- Main.asm
+++ Main.asm
@@ -9,11 +9,23 @@ Point ENDS
 .data
 	inputChar BYTE ?
 	player Point <40, 27>
+	coin Point<?,?>
 	dirX SBYTE 0
 	clock DWORD 0
+	ground BYTE "----------------------------------------------------------------------------------------------------------------------",0
+	isJumping BYTE 0
+	jumpCount BYTE 0 ; Used to keep track of how many frames till jump triggered
 .code
 	main PROC
+		;initialize stuff
+		call Randomize
+		call initializeground
 		call DrawPlayer
+		;call CreateRandomCoin
+		;call DrawCoin
+		
+
+
 		gameLoop:
 			call Update
 			jmp gameLoop 
@@ -21,21 +33,44 @@ Point ENDS
 		exit
 	main ENDP
 
+	initializeground PROC 
+		mov dl,0
+		mov dh,28
+		call Gotoxy
+		mov edx,OFFSET ground
+		mov eax,green (green * 16)
+		call SetTextColor
+		call WriteString
+		mov eax,white (black * 16)
+		call SetTextColor
+	initializeground ENDP
+
 	Update PROC
 		call HandleEvents
-		call Gravity
-		call Inertia
+
+		mov al, isJumping
+		cmp isJumping, 0
+		je _
+		call Jump
+		_:
+			call Gravity
+			call Inertia
 		ret
 	Update ENDP
 
 	Gravity PROC uses eax
 		movzx eax, player.y
-		cmp player.y, 27
-		jl _
+		cmp isJumping, 0
+		je CheckIfOnPlatform
 		ret
+		CheckIfOnPlatform:
+			cmp player.y, 27
+			jl _
+			ret
 		_:
 			call clearPlayer
 			inc player.y
+			call SideCollision
 			cmp dirX, 0
 			je Rest
 
@@ -49,6 +84,8 @@ Point ENDS
 			call delay
 			ret
 	Gravity ENDP
+
+	
 
 	Inertia PROC
 		inc Clock     ; i have used a clock to basically check if it has been 10 cycles without any movement
@@ -70,7 +107,11 @@ Point ENDS
 			_1:
 				cmp inputChar,"w"	;if w
 				jne _2
-				call Jump
+				mov al, isJumping
+				cmp isJumping, 1
+				je _4
+				mov isJumping, 1
+				je _4
 
 			_2:
 				cmp inputChar,"a" ;else if a
@@ -86,22 +127,27 @@ Point ENDS
 				ret			 ; else return
 	HandleEvents ENDP
 
-	Jump PROC uses ecx eax 
-		mov ecx, 5
-		_:
+	Jump PROC uses eax
 			call ClearPlayer
 			dec player.y
 			mov al,player.x 
 			add al, dirX
 			mov player.x, al
 			call DrawPlayer
+			call SideCollision
 			mov eax,40
 			call Delay
-			loop _
-		ret
+			inc jumpCount
+			cmp jumpCount, 5   ; If it has been 5 cycles since ju,p triggered
+			jne _			;if it hasnt then return	
+			mov jumpCount, 0	; else reset count
+			mov isJumping, 0	; Now player can jump again
+			_:
+				ret
 	Jump ENDP
 
 	moveLeft PROC
+		call SideCollision
 		mov clock, 0
 		call clearPlayer
 		mov dirX, -1
@@ -111,6 +157,7 @@ Point ENDS
 	moveLeft ENDP
 	
 	moveRight PROC
+		call SideCollision
 		mov clock, 0
 		call clearPlayer
 		mov dirX, 1
@@ -121,6 +168,8 @@ Point ENDS
 	
 
 	DrawPlayer PROC
+		mov eax,white (black * 16)
+		call settextcolor
 		mov dl, player.x
 		mov dh, player.y
 		mov al, 'X'
@@ -142,4 +191,45 @@ Point ENDS
 		call writeChar
 		ret
 	WriteCharToConsoleXY ENDP
+
+SideCollision Proc
+	cmp player.x,3
+	jg checkpos
+	call clearPlayer
+	mov player.x,3
+
+	checkpos:
+	cmp player.x,115
+	jl return
+	call clearPlayer
+	mov player.x,110
+
+	return:
+	ret
+SideCollision ENDP
+
+DrawCoin PROC
+	mov eax,yellow (yellow * 16)
+	call SetTextColor
+	mov dl,coin.x
+	mov dh,coin.y
+	call Gotoxy
+	mov al,"X"
+	call WriteChar
+	ret
+DrawCoin ENDP
+
+CreateRandomCoin PROC
+	mov eax,30
+	call RandomRange
+	mov coin.x,al
+	mov eax,5
+	call RandomRange
+	add al, 23
+	mov coin.y,al
+	ret
+CreateRandomCoin ENDP
+	
+
+
 END main
